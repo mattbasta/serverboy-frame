298e3b4caf6f1bc3633f0ff10f8817d844aeb919
bastawhiz <bastawhiz@7227f842-22fa-11df-8901-e16673284e38>
2010-02-26 17:26:50 +0000

    Added latest code

---
+++ trunk/0.3/all.php
@@ -0,0 +1,66 @@
+<?
+
+function closemem() {
+	global $memcache;
+	if(isset($memcache))
+		$memcache->close();
+}
+register_shutdown_function('closemem');
+
+$memcache = new Memcache();
+$memcache->connect('localhost', 11211) or die('Cannot connect to memcache');
+
+$memcache->increment('services/frame/total');
+$memcache->increment('services/frame/0.3/total');
+
+
+if(!defined('PATH_PREFIX'))
+	define('PATH_PREFIX', './');
+
+require('../cssmin.php');
+
+ob_start("ob_gzhandler");
+
+header('Content-type: text/css');
+
+header("Expires: Tue, 01 Dec 2037 16:00:00 GMT");
+header("Cache-Control: public");
+
+if(isset($_GET['include'])) {
+	$includes = explode(',', $_GET['include']);
+} else {
+	$includes = array(
+		'reset',
+		'formatting',
+		'code',
+		'base_typography',
+		'typography',
+		'layout',
+		'controls',
+		'forms',
+		'tables',
+		'form_layout',
+		'paper_layout',
+		'print'
+	);
+}
+$output = '';
+$lastMod = 0;
+foreach($includes as $include) {
+	$output .= cssmin::minify(file_get_contents(PATH_PREFIX . "$include.css"));
+	$lastMod = max($lastMod, filemtime(PATH_PREFIX . "$include.css"));
+}
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $lastMod) . ' GMT');   
+$output = str_replace(';}','}',$output);
+$output = trim($output);
+
+$header = <<<HEADER
+/*
+Serverboy Frame
+Copyright 2010 Matt Basta
+Build 0.3 (Min)
+*/
+HEADER;
+$output = $header . $output;
+
+echo $output;
