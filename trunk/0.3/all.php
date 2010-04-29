51bce9798e7b900cfd694cbd0a19a0c6b0e056f1
bastawhiz <bastawhiz@7227f842-22fa-11df-8901-e16673284e38>
2010-04-29 20:51:29 +0000

    Lots of bug fixes.

--- trunk/0.3/all.php
+++ trunk/0.3/all.php
@@ -17,14 +17,11 @@ $memcache->increment('services/frame/0.3/total');
 if(!defined('PATH_PREFIX'))
 	define('PATH_PREFIX', './');
 
-require('../cssmin_2.php');
-
-ob_start("ob_gzhandler");
-
 header('Content-type: text/css');
 
 header("Expires: Tue, 01 Dec 2037 16:00:00 GMT");
-header("Cache-Control: public");
+header("Cache-Control: public, max-age=31536000");
+header('Frame: Very Yes');
 
 if(isset($_GET['include'])) {
 	$includes = explode(',', $_GET['include']);
@@ -44,16 +41,28 @@ if(isset($_GET['include'])) {
 		'print'
 	);
 }
-$output = '';
 $lastMod = 0;
-foreach($includes as $include) {
-	$output .= file_get_contents(PATH_PREFIX . "$include.css");
+foreach($includes as $include)
 	$lastMod = max($lastMod, filemtime(PATH_PREFIX . "$include.css"));
+
+if(isset($_SERVER["HTTP_IF_MODIFIED_SINCE"])) {
+	$expected_modified = strtotime(preg_replace('/;.*$/','',$_SERVER["HTTP_IF_MODIFIED_SINCE"]));
+	if($lastMod <= $expected_modified) {
+		header("HTTP/1.0 304 Not Modified");
+		exit;
+	}
 }
 
+$output = '';
+foreach($includes as $include)
+	$output .= file_get_contents(PATH_PREFIX . "$include.css");
+
+require('../cssmin_2.php');
 $output = Minify_CSS_Compressor::process($output);
 
-header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $lastMod) . ' GMT');   
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $lastMod) . ' GMT');
+header('Age: ' . ((time() - $lastMod) % 31536000));
+
 $output = str_replace("\n",' ',$output);
 $output = trim($output);
 
@@ -66,4 +75,5 @@ Build 0.3 (Min)
 HEADER;
 $output = $header . $output;
 
+ob_start("ob_gzhandler");
 echo $output;
